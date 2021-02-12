21e26b3753f3b17882f948f91553a16aead76da4
gregorem-lap <grzegorz.musial.personal@gmail.com>
2021-02-12 03:03:15 -0800

    Chat server working on osapi

--- myProject_linux/01_server/server.cpp
+++ myProject_linux/01_server/server.cpp
@@ -7,6 +7,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "CUnit/CUnitCI.h"
+#include "osapi.h"
+#include <climits>
 
 void error(const char *msg)
 {
@@ -14,18 +17,114 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
+class ClientThread : public osapi::Thread
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     // create a socket
+public:
+    ClientThread() :
+        Thread(3, 512, osapi::JOINABLE)
+    {
+        bzero(buffer,256);
+    }
+    void SetSocketId(int soc){
+        socket_id = soc;
+    }
+
+    int GetMessage(char * buf){
+        int len_msg = 0;
+        
+        if(!isRunning()){
+            return 0;
+        }
+
+        if((len_msg = strlen(buffer)) != 0){
+            strcpy(buf, buffer);
+            bzero(buffer,256);
+            if(false == mutex_client.lock(1)){
+                mutex_client.unlock();
+            }
+        }
+        
+        return len_msg;
+    }
+
+    void SendMessage(char *data){
+        if(isRunning()){
+            send(socket_id, data, strlen(data), 0);
+        }
+    }
+private:
+    int socket_id;
+    char buffer[256];
+    int bytes_read;
+    osapi::Mutex mutex_client;
+    virtual void body() {
+        while(1){
+            if(mutex_client.lock(UINT_MAX)){
+                bytes_read = read(socket_id,buffer,255);
+                if(bytes_read == 0){
+                    printf("Disconnect client\n");
+                    break;
+                }
+            }
+        }   
+        close(socket_id);
+    }
+};
+
+class AcceptThread : public osapi::Thread
+{
+public:
+    AcceptThread() : 
+        Thread(3, 512, osapi::JOINABLE) 
+    { 
+        sock_id = -1;
+        readed_sock = false;
+    }
+    void SetParameters(int sock_, socklen_t clilen_, struct sockaddr_in cli_addr_){
+        sockfd = sock_;
+        clilen = clilen_;
+        cli_addr = cli_addr_;
+    }
+    int GetSocketId(void){
+        int sock_temp = -1;
+        if(sock_id > 0 ){
+            if(false == mutex_accept.lock(1)){
+                sock_temp = sock_id;
+                sock_id = -1;
+                mutex_accept.unlock();
+                return sock_temp;
+            }
+        }
+        return sock_temp;
+    }
+private:
+    int sockfd;
+    int sock_id;
+    bool readed_sock;
+    osapi::Mutex mutex_accept;
+    socklen_t clilen;
+    struct sockaddr_in cli_addr;
+    virtual void body() {
+        while(1){
+            if(mutex_accept.lock(UINT_MAX)){
+                sock_id = accept(sockfd, 
+                    (struct sockaddr *) &cli_addr, &clilen);
+                printf("server: got connection from %s port %d\n",
+                        inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+            }
+        }   
+    }
+};
+
+
+class ServerThread : public osapi::Thread
+{
+public:
+    ServerThread(int port) : 
+        Thread(3, 512, osapi::JOINABLE),  
+        portno(port)
+    { 
+        // create a socket
      // socket(int domain, int type, int protocol)
      sockfd =  socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
@@ -33,8 +132,6 @@ int main(int argc, char *argv[])
 
      // clear address structure
      bzero((char *) &serv_addr, sizeof(serv_addr));
-
-     portno = atoi(argv[1]);
 
      /* setup the host_addr structure for use in bind call */
      // server byte order
@@ -50,38 +147,67 @@ int main(int argc, char *argv[])
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
 
-     listen(sockfd,5);
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    accept.SetParameters(sockfd, clilen, cli_addr);
+    accept.run();
+    num_client = 0;
+    
+    run(); 
+    }
+    ~ServerThread(){
+        close(sockfd);
+    }
+private:
+    int sockfd, newsockfd; 
+    int portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    ClientThread client[5];
+    int num_client;
+    AcceptThread accept;
+    virtual void body() {
 
-     // The accept() call actually accepts an incoming connection
-
-     
-        clilen = sizeof(cli_addr);
-
-        newsockfd = accept(sockfd, 
-                    (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0) 
-            error("ERROR on accept");
-
-        printf("server: got connection from %s port %d\n",
-                inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-
-
-    while(1){
-        // This send() function sends the 13 bytes of the string to the new socket
-        bzero(buffer,256);
-        n = read(newsockfd,buffer,255);
-        if (n > 0){
-            if(!strcmp(buffer, "exit\n")){
-                printf("exit command\n");
-                break;
+        while(1){
+           //check new connections
+            if((sockfd = accept.GetSocketId()) != -1){
+                printf("New connection! %d\n", sockfd);
+                client[num_client].SetSocketId(sockfd);
+                client[num_client].run();
+                ++num_client;
             }
 
-            send(newsockfd, buffer, strlen(buffer), 0);
+            //check disconnect clients
+            /*
+            for(int i=0;i<num_client;++i){
+                if(!client[i].isRunning()){
+                    --num_client;
+
+                }
+            }
+            */
+
+            //send to all clients
+            for(int i=0;i<num_client;++i){
+               if(client[i].GetMessage(buffer)){
+                   for(int j=0;j<num_client;++j){
+                       if(i != j){
+                           client[j].SendMessage(buffer);
+                       }
+                   }
+               }
+           }
         }
+        
+    }
+};
 
-     }
+int main(int argc, char *argv[])
+{
+    ServerThread server(1007);
+    server.join(UINT_MAX);
 
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+    return 0; 
 }
--- myProject_linux/02_client/client.cpp
+++ myProject_linux/02_client/client.cpp
@@ -6,6 +6,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include "CUnit/CUnitCI.h"
+#include "osapi.h"
+#include <climits>
 
 void error(const char *msg)
 {
@@ -13,47 +16,112 @@ void error(const char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
+class ReadClientThread : public osapi::Thread
 {
+public:
+    ReadClientThread() :
+        Thread(3, 2512, osapi::JOINABLE)
+    {
+    }
+    void SetSocketId(int soc){
+        sockfd = soc;
+    }
+private:
+    int n; 
+    char buffer[256];
+    char line[1024];
+    int sockfd;
+    virtual void body() {
+        while(1){
+            bzero(buffer,256);
+            n = read(sockfd, buffer, 255);
+            if (n < 0) 
+                error("ERROR reading from socket");
+            //scanf("%1023[^\n]", line);
+            printf("%s", buffer);
+        } 
+    }
+};
+
+class WriteClientThread : public osapi::Thread
+{
+public:
+    WriteClientThread() :
+        Thread(3, 512, osapi::JOINABLE)
+    {
+    }
+    void SetSocketId(int soc){
+        sockfd = soc;
+    }
+private:
+    int n;
+    char buffer[256];
+    int sockfd;
+    virtual void body() {
+        while(1){
+            printf("Please enter the message: ");
+            bzero(buffer,256);
+            fgets(buffer,255,stdin);
+            n = write(sockfd, buffer, strlen(buffer));
+            if (n < 0) 
+                error("ERROR writing to socket");
+        }
+    }
+};
+
+
+class ClientThread : public osapi::Thread
+{
+public:
+    ClientThread(int port, const char *hostname) :
+        Thread(3, 512, osapi::JOINABLE), 
+        portno(port), 
+        server(gethostbyname(hostname))
+    {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) 
+            error("ERROR opening socket");
+        if (server == NULL) {
+            fprintf(stderr,"ERROR, no such host\n");
+            exit(0);
+        }
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+        serv_addr.sin_port = htons(portno);
+        if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+            error("ERROR connecting");
+        bzero(buffer,256);
+        run();
+    }
+
+private:
+    char buffer[256];
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    ReadClientThread read;
+    WriteClientThread write;
+    virtual void body() {
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+        read.SetSocketId(sockfd);
+        write.SetSocketId(sockfd);
+
+        read.run();
+        write.run();
+
+        read.join(UINT_MAX);
+        write.join(UINT_MAX);
+
+        close(sockfd); 
     }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    while(1){
-        printf("Please enter the message: ");
-        bzero(buffer,256);
-        fgets(buffer,255,stdin);
-        n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0) 
-            error("ERROR writing to socket");
-        bzero(buffer,256);
-        n = read(sockfd, buffer, 255);
-        if (n < 0) 
-            error("ERROR reading from socket");
-        printf("%s\n", buffer);
-    }
-    close(sockfd);
+};
+
+int main(int argc, char *argv[])
+{
+    ClientThread client(1007, "127.0.0.1");
+    client.join(UINT_MAX);
     return 0;
 }
